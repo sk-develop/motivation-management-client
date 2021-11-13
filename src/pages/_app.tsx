36c6a9d7e9c80d93efae25b4d410ad0d96ec824f
sk <v22w6y@gmail.com>
2021-11-13 20:02:36 +0900

    Setup Next.js with Tailwind CSS and TypeScript

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import "modern-css-reset/dist/reset.min.css";
+import "tailwindcss/tailwind.css";
+
+function MyApp({ Component }) {
+  return <Component />;
+}
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1 className="text-xl text-red-500">Welcome to Tailwind CSS!</h1>;
+};
+
+export default Home;
